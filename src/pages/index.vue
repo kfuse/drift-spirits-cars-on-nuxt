d508677dd6c0a805f55ca30ffa25a1465764c8c4
kfuse <k_fuse1123@yahoo.co.jp>
2020-07-02 19:06:53 +0900

    add h1 element

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    index
+    <h1 class="mainTitle">ドリフトスピリッツ 車種一覧</h1>
   </div>
 </template>
+
+<style scoped>
+.mainTitle {
+  margin: 0 0 20px;
+  padding: 40px 20px;
+  border-bottom: 1px solid #eee;
+  color: #666;
+}
+</style>
