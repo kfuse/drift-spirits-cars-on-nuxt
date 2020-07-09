f39ca60ace597c7bc99a98da6f0c47a6641bb480
kfuse <k_fuse1123@yahoo.co.jp>
2020-07-09 18:48:29 +0900

    add footer component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,17 +4,20 @@
     <div id="content">
       <setting-pane />
     </div>
+    <global-footer />
   </div>
 </template>
 
 <script>
-import globalHeader from '~/components/Molecules/header'
+import globalHeader from '~/components/Organisms/header'
 import settingPane from '~/components/Organisms/setting-pane'
+import globalFooter from '~/components/Organisms/footer'
 
 export default {
   components: {
     'global-header': globalHeader,
-    'setting-pane': settingPane
+    'setting-pane': settingPane,
+    'global-footer': globalFooter
   }
 }
 </script>
